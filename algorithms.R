6f73837c620ebda699b75e02cf99dbbd0f5f1a45
Hardis, Richard Phillip <rhardis3@gatech.edu>
2020-04-04 08:58:21 -0400

    new algs
    new algs

--- algorithms.R
+++ algorithms.R
@@ -3,6 +3,22 @@
 ###
 library(glmnet)
 
+regression_simp = function(features_train, features_test, target_train, target_test){
+  x_vars = as.matrix(features_train)
+  y_var = target_train
+  set.seed(7406)
+  
+  train.control <- trainControl(method = "repeatedcv", 
+                                number = 10, repeats = 10)
+  Regression <- train(y_var ~ (x_vars)^2,              #we can do this, or stepAIC for all comb.
+                      data = cbind(x_vars, y_var), 
+                      method = "lm",
+                      trControl = train.control)
+  
+  prediction = predict(Regression, target_test)
+  return(prediction)
+}
+
 lasso_reg = function(features_train, features_test, target_train, target_test){
   x_vars = as.matrix(features_train)
   y_var = target_train
@@ -36,5 +52,37 @@ ridge_reg = function(features_train, features_test, target_train, target_test){
   y_test = target_test
   
   prediction = predict(best_model, s=best_lam, newx=x_test)
+  return(prediction)
+}
+
+boosted_reg = function(features_train, features_test, target_train, target_test){
+  x_vars = as.matrix(features_train)
+  y_var = target_train
+  set.seed(7406)
+  train.control <- trainControl(method = "repeatedcv", 
+                                number = 10)
+  # Train the model
+  BoostedRegressionTree <- train(y_var ~ x_vars, 
+                                 data = cbind(features_train, target_train), 
+                                 method = "xgbTree",
+                                 trControl = train.control)
+  
+  prediction <- BoostedRegressionTree %>% predict(features_test)
+  return(prediction)
+}
+
+MARS = function(features_train, features_test, target_train, target_test){
+  x_vars = as.matrix(features_train)
+  y_var = target_train
+  set.seed(7406)
+  train.control <- trainControl(method = "repeatedcv", 
+                                number = 10, repeats = 10)
+  # Train the model
+  MARScv <- train(y_var ~ x_vars,
+                      data = cbind(features_train, target_train),
+                      method = "earth",
+                      trControl = train.control)
+  
+  prediction <- MARScv %>% predict(features_test)
   return(prediction)
 }
